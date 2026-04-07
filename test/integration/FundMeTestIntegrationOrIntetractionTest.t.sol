3e4086171948bb913d6d985f05f4a2f243765f42
Nathaniel Ifeanyi Maduchem <ifeanyimaduchemm@gmail.com>
2026-04-08 00:46:16 +0100

    refactor: temporary disabled integration-test (#1)

--- test/integration/FundMeTestIntegrationOrIntetractionTest.t.sol
+++ test/integration/FundMeTestIntegrationOrIntetractionTest.t.sol
@@ -1,61 +1,61 @@
 // SPDX-License-Identifier: MIT
 
 
-pragma solidity ^0.8.18;
+// pragma solidity ^0.8.18;
 
 
-// import {Test} from "forge-std/Test.sol";
-import {Test, console} from "forge-std/Test.sol";
+// // import {Test} from "forge-std/Test.sol";
+// import {Test, console} from "forge-std/Test.sol";
 
 
-//IMPORTING OUR CONTRACTS OUR CONTRACT WHICH WE WANT TO TEST (RATHER THAN TESTING BASIC TEST)
-import {FundMe} from "../../src/FundMe.sol";
+// //IMPORTING OUR CONTRACTS OUR CONTRACT WHICH WE WANT TO TEST (RATHER THAN TESTING BASIC TEST)
+// import {FundMe} from "../../src/FundMe.sol";
 
-import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+// import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
-import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
-
-
-contract InteractionsTest is Test{ 
-
-     /**
-     * STATE VARIABLE TO HOLD THE INSTANCE OF OUR CONTRACT "FUNDME" WHICH WE WANT TO TEST. WE SCOOPED IT , TO BE VISIBLE TO THE OVERALL CONTRACT.
-     */
-    FundMe fundMe;
+// import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 
-    // TO CREATE A FAKE-USER USING FOUNDRY CHEATCODE (TO MAKE A TEST)
-    address USER = makeAddr("user");
+// contract InteractionsTest is Test{ 
 
-    uint256 constant  SEND_VALUE = 0.1 ether; // 100000000000000000
-    uint256 constant STARTING_BALANCE = 10 ether;
-
-    uint256  constant GAS_PRICE = 1;
-
-
-    function setUp()  external {
-        DeployFundMe deployFundMe = new DeployFundMe();
-        fundMe = deployFundMe.run();
+//      /**
+//      * STATE VARIABLE TO HOLD THE INSTANCE OF OUR CONTRACT "FUNDME" WHICH WE WANT TO TEST. WE SCOOPED IT , TO BE VISIBLE TO THE OVERALL CONTRACT.
+//      */
+//     FundMe fundMe;
 
 
-    //     DeployFundMe deployFundMe = new DeployFundMe();
-    //    fundMe = deployFundMe.run();
-        vm.deal(USER, STARTING_BALANCE); 
-    }
+//     // TO CREATE A FAKE-USER USING FOUNDRY CHEATCODE (TO MAKE A TEST)
+//     address USER = makeAddr("user");
+
+//     uint256 constant  SEND_VALUE = 0.1 ether; // 100000000000000000
+//     uint256 constant STARTING_BALANCE = 10 ether;
+
+//     uint256  constant GAS_PRICE = 1;
 
 
-    function testUserCanFundInteractions() public {
-        FundFundMe fundFundMe = new FundFundMe();
-        // vm.prank(USER);
-        // vm.deal(USER, 1e18);
-        fundFundMe.fundFundMe(address(fundMe));
+//     function setUp()  external {
+//         DeployFundMe deployFundMe = new DeployFundMe();
+//         fundMe = deployFundMe.run();
 
-        // address funder = fundMe.getFunder(0);
-        // assertEq(funder, USER);
 
-        WithdrawFundMe withdrawFundMe =  new WithdrawFundMe();
-        withdrawFundMe.withdrawFundMe(address(fundMe));
-        assert(address(fundMe).balance == 0);
-    }
+//     //     DeployFundMe deployFundMe = new DeployFundMe();
+//     //    fundMe = deployFundMe.run();
+//         vm.deal(USER, STARTING_BALANCE); 
+//     }
 
-}
+
+//     function testUserCanFundInteractions() public {
+//         FundFundMe fundFundMe = new FundFundMe();
+//         // vm.prank(USER);
+//         // vm.deal(USER, 1e18);
+//         fundFundMe.fundFundMe(address(fundMe));
+
+//         // address funder = fundMe.getFunder(0);
+//         // assertEq(funder, USER);
+
+//         WithdrawFundMe withdrawFundMe =  new WithdrawFundMe();
+//         withdrawFundMe.withdrawFundMe(address(fundMe));
+//         assert(address(fundMe).balance == 0);
+//     }
+
+// }
